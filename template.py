135c5ace12658cbabfe40ce725cf0a0de20f115c
Burning0223 <liuboning2004@126.com>
2026-09-14 00:06:21 +0800

    Change it to qwenbase

--- template.py
+++ template.py
@@ -5,12 +5,12 @@ class Template:
     format_user: str
     format_assistant: str
     def format(self,user_content,assistant_content):
-        prompt=self.format_user.replace("{{content}}",user_content)
-        response=self.format_assistant.replace("{{content}}",assistant_content)
+        prompt=self.format_user.replace("{content}",user_content)
+        response=self.format_assistant.replace("{content}",assistant_content)
         full_text=prompt+response
         return prompt,full_text
     def format_prompt(self,user_content):
-        prompt=self.format_user.replace("{{content}}",user_content)
+        prompt=self.format_user.replace("{content}",user_content)
         return prompt
 def register_template(name,format_user,format_assistant):
     if name in TEMPLATES:
@@ -20,5 +20,6 @@ def register_template(name,format_user,format_assistant):
     
 register_template(
     name="qwen",
-    format_user="<|im_start|>user\n{{content}}<|im_end|>\n<|im_start|>assistant\n",
-    format_assistant="{{content}}<|im_end|>\n",)
+    format_user="{content}",
+    format_assistant="{content}<|endoftext|>\n",
+)
