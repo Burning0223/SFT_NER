ad88227222c597906dec31a58814c451eba62adb
Burning0223 <liuboning2004@126.com>
2026-09-13 22:09:00 +0800

    Refactor the model loading logic

--- model.py
+++ model.py
@@ -1,34 +1,42 @@
 import torch
 from transformers import AutoModelForCausalLM,BitsAndBytesConfig
-from peft import LoraConfig,TaskType,get_peft_model,prepare_model_for_kbit_training
+from peft import LoraConfig,TaskType,get_peft_model,prepare_model_for_kbit_training,PeftModel
 class NER_SFT(torch.nn.Module):
-    def __init__(self,arg):
+    def __init__(self,arg,tokenizer):
         super().__init__()
         self.arg=arg
+        self.tokenizer=tokenizer
         lora_config=LoraConfig(task_type=TaskType.CAUSAL_LM,
                                            r=arg.lora_r,
                                            lora_alpha=arg.lora_alpha,
                                            lora_dropout=arg.lora_dropout,
                                            target_modules=arg.target_modules)
-        if arg.peft_method=="lora":
-            self.model=AutoModelForCausalLM.from_pretrained(arg.model_path)
-            self.model=get_peft_model(self.model,lora_config)
-        elif arg.peft_method=="qlora":
+        self.model=self.load_base_model()
+        self.model=get_peft_model(self.model,lora_config)
+    def load_base_model(self):
+        if self.arg.peft_method=="lora":
+            base_model=AutoModelForCausalLM.from_pretrained(self.arg.model_path,torch_dtype=torch.bfloat16)
+        elif self.arg.peft_method=="qlora":
             bnb_config=BitsAndBytesConfig(
                 load_in_4bit=True,
                 bnb_4bit_use_double_quant=True,
                 bnb_4bit_compute_dtype=torch.float16,
                 bnb_4bit_quant_type="nf4"
             )
-            self.model=AutoModelForCausalLM.from_pretrained(
-                arg.model_path,
+            base_model=AutoModelForCausalLM.from_pretrained(
+                self.arg.model_path,
                 quantization_config=bnb_config
             )
-            self.model=prepare_model_for_kbit_training(self.model)
-            self.model=get_peft_model(self.model,lora_config)
+            base_model=prepare_model_for_kbit_training(base_model,use_gradient_checkpointing=True)
         else:
-            self.model=AutoModelForCausalLM.from_pretrained(arg.model_path)
-
+            raise ValueError(
+                f"不支持的peft_method: {self.arg.peft_method}"
+            )
+        return base_model
+    def load_best_model(self,best_model_path):
+        base_model=self.load_base_model()
+        self.model=PeftModel.from_pretrained(base_model, best_model_path, is_trainable=False)
+    
     def forward(self,input_ids,attention_mask,labels=None):
         if labels is not None:
             outputs=self.model(input_ids=input_ids,attention_mask=attention_mask,labels=labels)
